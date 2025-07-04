c07115aeeeb74a70ea004c427737aa966fc43033
devops979 <devops979@gmail.com>
2025-07-04 16:54:16 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,10 @@
-// Placeholder for models.py
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    department = db.Column(db.String(50))
+
+    def to_dict(self):
+        return {"id": self.id, "name": self.name, "department": self.department}
