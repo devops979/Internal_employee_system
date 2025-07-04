29fac451e48e71712212715ac98213af5c1cb544
devops979 <devops979@gmail.com>
2025-07-04 17:10:09 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,3 @@
-// Placeholder for outputs.tf
+output "cluster_name" { value = module.eks.cluster_name }
+output "cluster_endpoint" { value = module.eks.cluster_endpoint }
+output "db_endpoint" { value = module.rds.db_endpoint }
