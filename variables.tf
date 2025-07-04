2332845f271f16bdc3acb27e0b4d24d0e0edd5ea
devops979 <devops979@gmail.com>
2025-07-04 17:10:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
-// Placeholder for variables.tf
+variable "region" { default = "us-east-1" }
+variable "project" { default = "support-portal" }
+
+variable "cluster_name" { default = "support-eks" }
+
+variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "azs" { type = list(string) }
+variable "public_subnet_cidrs" { type = list(string) }
+variable "private_subnet_cidrs" { type = list(string) }
+
+variable "db_name" { default = "employees" }
+variable "db_engine" { default = "postgres" }
