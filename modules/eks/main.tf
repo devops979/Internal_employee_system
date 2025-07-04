dd25ccecd2faaab4f96f8e1e8fb466a06239baa0
devops979 <devops979@gmail.com>
2025-07-04 17:03:47 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,1 +1,65 @@
-// Placeholder for main.tf
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.this.arn
+  vpc_config {
+    subnet_ids = concat(var.private_subnet_ids, var.public_subnet_ids)
+  }
+  version = "1.29"
+}
+
+data "aws_iam_policy_document" "cluster_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals { type = "Service"; identifiers = ["eks.amazonaws.com"] }
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = "${var.cluster_name}-eks-role"
+  assume_role_policy = data.aws_iam_policy_document.cluster_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Managed node group for backend pods
+resource "aws_eks_node_group" "backend_nodes" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "backend-nodes"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.private_subnet_ids
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+  instance_types = ["t3.medium"]
+}
+
+data "aws_iam_policy_document" "node_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals { type = "Service"; identifiers = ["ec2.amazonaws.com"] }
+  }
+}
+
+resource "aws_iam_role" "node" {
+  name               = "${var.cluster_name}-node-role"
+  assume_role_policy = data.aws_iam_policy_document.node_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+output "cluster_name" { value = aws_eks_cluster.this.name }
+output "cluster_endpoint" { value = aws_eks_cluster.this.endpoint }
+output "oidc_provider_arn" { value = aws_eks_cluster.this.identity[0].oidc[0].issuer }
