4fa9137a28a00f299dc27523ada89610eeca9df1
devops979 <devops979@gmail.com>
2025-07-04 16:56:36 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,6 @@
-// Placeholder for index.js
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
