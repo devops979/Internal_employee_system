0e874c2f7c069c152a7e724eefde144607406e0a
devops979 <devops979@gmail.com>
2025-07-04 17:22:07 +0530

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,1 +1,3 @@
-// Placeholder for outputs.tf
+output "cluster_name" { value = aws_eks_cluster.this.name }
+output "cluster_endpoint" { value = aws_eks_cluster.this.endpoint }
+output "oidc_provider_arn" { value = aws_eks_cluster.this.identity[0].oidc[0].issuer }
