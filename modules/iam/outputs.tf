fe62122bb67127293711e492647527dc94aa079d
devops979 <devops979@gmail.com>
2025-07-04 17:26:07 +0530

    Update outputs.tf

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,1 +1,14 @@
-// Placeholder for outputs.tf
+output "irsa_role_name" {
+  description = "The name of the IAM role used for IRSA"
+  value       = aws_iam_role.irsa_secrets.name
+}
+
+output "irsa_role_arn" {
+  description = "The ARN of the IAM role used for IRSA"
+  value       = aws_iam_role.irsa_secrets.arn
+}
+
+output "irsa_assume_role_policy" {
+  description = "The assume role policy document for IRSA"
+  value       = data.aws_iam_policy_document.irsa_assume.json
+}
