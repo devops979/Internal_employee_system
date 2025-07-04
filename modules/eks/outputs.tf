8b152c4d89c015adabeee618d4a24fea466f83e8
devops979 <devops979@gmail.com>
2025-07-04 17:25:02 +0530

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,3 +1,34 @@
-output "cluster_name" { value = aws_eks_cluster.this.name }
-output "cluster_endpoint" { value = aws_eks_cluster.this.endpoint }
-output "oidc_provider_arn" { value = aws_eks_cluster.this.identity[0].oidc[0].issuer }
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = aws_eks_cluster.this.name
+}
+
+output "eks_cluster_arn" {
+  description = "The ARN of the EKS cluster"
+  value       = aws_eks_cluster.this.arn
+}
+
+output "eks_cluster_endpoint" {
+  description = "The endpoint for the EKS cluster"
+  value       = aws_eks_cluster.this.endpoint
+}
+
+output "eks_cluster_version" {
+  description = "The Kubernetes version of the EKS cluster"
+  value       = aws_eks_cluster.this.version
+}
+
+output "eks_cluster_role_arn" {
+  description = "The ARN of the IAM role used by the EKS cluster"
+  value       = aws_iam_role.this.arn
+}
+
+output "node_group_name" {
+  description = "The name of the managed node group"
+  value       = aws_eks_node_group.backend_nodes.node_group_name
+}
+
+output "node_group_role_arn" {
+  description = "The ARN of the IAM role used by the node group"
+  value       = aws_iam_role.node.arn
+}
