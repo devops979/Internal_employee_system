df823ed58e17c23a326583924330c6bb596bf2ea
devops979 <devops979@gmail.com>
2025-07-04 17:06:26 +0530

    Update main.tf

--- modules/fargate_profile/main.tf
+++ modules/fargate_profile/main.tf
@@ -1,1 +1,27 @@
-// Placeholder for main.tf
+resource "aws_eks_fargate_profile" "this" {
+  cluster_name           = var.cluster_name
+  fargate_profile_name   = "${var.namespace}-fargate"
+  subnet_ids             = var.subnet_ids
+  pod_execution_role_arn = aws_iam_role.fargate.arn
+
+  selector {
+    namespace = var.namespace
+  }
+}
+
+data "aws_iam_policy_document" "fargate_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals { type = "Service"; identifiers = ["eks-fargate-pods.amazonaws.com"] }
+  }
+}
+
+resource "aws_iam_role" "fargate" {
+  name               = "${var.namespace}-fargate-role"
+  assume_role_policy = data.aws_iam_policy_document.fargate_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "fargate" {
+  role       = aws_iam_role.fargate.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+}
