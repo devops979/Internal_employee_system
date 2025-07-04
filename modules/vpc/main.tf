80a718808d114b82b03a4e764840fd8573214cb4
devops979 <devops979@gmail.com>
2025-07-04 17:35:20 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "this" {
   tags = { Name = var.name }
 }
 
-resource "aws_internet_gateway" "igw" {
+resource "aws_internet_gateway" "igw" {a
   vpc_id = aws_vpc.this.id
   tags = { Name = "${var.name}-igw" }
 }
@@ -27,6 +27,68 @@ resource "aws_subnet" "private" {
   tags = { Name = "${var.name}-private-${each.key}" }
 }
 
-output "vpc_id" { value = aws_vpc.this.id }
-output "public_subnet_ids" { value = [for s in aws_subnet.public : s.id] }
-output "private_subnet_ids" { value = [for s in aws_subnet.private : s.id] }
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.name}-nat-eip"
+  }
+}
+
+# NAT Gateway in a public subnet (using the first public subnet)
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = values(aws_subnet.public)[0].id
+
+  tags = {
+    Name = "${var.name}-nat-gw"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.name}-public-rt"
+  }
+}
+
+# Associate public subnets with public route table
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.name}-private-rt"
+  }
+}
+
+# Associate private subnets with private route table
+resource "aws_route_table_association" "private" {
+  for_each = aws_subnet.private
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
+
