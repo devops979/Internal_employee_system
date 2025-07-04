399f5f80f69303633dcd22add7b224f621056d9f
devops979 <devops979@gmail.com>
2025-07-04 17:23:45 +0530

    Update outputs.tf

--- modules/fargate_profile/outputs.tf
+++ modules/fargate_profile/outputs.tf
@@ -1,1 +1,19 @@
-// Placeholder for outputs.tf
+output "fargate_profile_name" {
+  description = "The name of the EKS Fargate profile"
+  value       = aws_eks_fargate_profile.this.fargate_profile_name
+}
+
+output "fargate_profile_arn" {
+  description = "The ARN of the EKS Fargate profile"
+  value       = aws_eks_fargate_profile.this.arn
+}
+
+output "fargate_iam_role_name" {
+  description = "The name of the IAM role used for Fargate pod execution"
+  value       = aws_iam_role.fargate.name
+}
+
+output "fargate_iam_role_arn" {
+  description = "The ARN of the IAM role used for Fargate pod execution"
+  value       = aws_iam_role.fargate.arn
+}
